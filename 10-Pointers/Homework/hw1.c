c632b72a1df62a11c2db834ffadc48adff9effeb
omeraslan <omeeraslan@hotmail.com>
2026-01-27 00:47:49 +0300

    +2 pointer homeworks

--- 10-Pointers/Homework/hw1.c
+++ 10-Pointers/Homework/hw1.c
@@ -2,9 +2,9 @@
 
 1) Adres ve boyut gözlemi
 
-• char, int, double türlerinden birer değişken tanımlayın; her biri için uygun pointer tanımlayın.
-• Her değişken için sizeof(değişken) ve sizeof(pointer) değerlerini yazdırın.
-• &x;, ptr ve &ptr; adreslerini %p ile yazdırın. (Not: adresler çalıştırmaya göre değişebilir.)
+• 1) char, int, double türlerinden birer değişken tanımlayın; her biri için uygun pointer tanımlayın.
+• 2) Her değişken için sizeof(değişken) ve sizeof(pointer) değerlerini yazdırın.
+• 3) &x;, ptr ve &ptr; adreslerini %p ile yazdırın. (Not: adresler çalıştırmaya göre değişebilir.)
 
 */
 
@@ -13,15 +13,17 @@
 
 int main(){
 
-    char c;
-    char *cp;
+    // 1. Adim
+    char c = 'A';
+    char *cp = &c;
 
-    int i;
-    int *ip;
+    int i = 1;
+    int *ip = &i;
 
-    double d;
-    double *dp;
+    double d = 3.14;
+    double *dp = &d;
 
+    // 2. Adim
     printf(">>\nc isimli siradan char degiskenin bellekteki boyutu %zu bye'dir.\n", sizeof(c));
     printf("cp isimli pointer char degiskenin bellekteki boyutu %zu byte'dir.\n\n", sizeof(cp));
 
@@ -31,6 +33,12 @@ int main(){
     printf(">>\nd isimli siradan double degiskenin bellekteki boyutu %zu byte'dir.\n", sizeof(d));
     printf("dp isimli pointer double degiskenin bellekteki boyutu %zu byte'dir.\n\n", sizeof(dp));
 
+    // 3. Adim
+    printf("--- Adres Bilgileri ---\n");
+
+    printf("DegiskenAdresi: (&c): %p, Pointer (cp): %p, PtrAdresi (&cp): %p\n", &c, cp, &cp);
+    printf("DegiskenAdresi: (&i): %p, Pointer (ip): %p, PtrAdresi (&ip): %p\n", &i, ip, &ip);
+    printf("DegiskenAdresi: (&d): %p, Pointer (dp): %p, PtrAdresi (&dp): %p\n", &d, dp, &dp);
 
 
     return 0;
--- 10-Pointers/Homework/hw2.c
+++ 10-Pointers/Homework/hw2.c
@@ -9,11 +9,37 @@
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#define LIST_SIZE 10
 
+void print_array(const int *p, int n){
+    for(int i = 0; i < n; i++){
+        printf("Deger: %d\n", *(p++));
+    }
+}
 
 int main(){
 
+    int numbers[LIST_SIZE];
+
+    int *ptr = numbers; // ya da "int *ptr = &numbers[0];"
+
+    for (int i = 0; i < LIST_SIZE; i++){
+        *(ptr + i) = i;
+    }
+
+    for (int i = 0; i < LIST_SIZE; i++){
+        printf(">> numbers[i] = %d\n", numbers[i]);
+    }
+
+    printf("\n");
+
+    for (int i = 0; i < LIST_SIZE; i++){
+        printf(">> *(numbers + i) = %d\n", *(numbers + i));
+    }
+
+    printf("\nprint_array() fonksiyonu ile:\n");
+    
+    print_array(ptr, LIST_SIZE);
 
     return 0;
 }
