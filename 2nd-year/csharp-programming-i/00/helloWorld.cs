a0a521c02e38d5c3d2c4e4fb900e24a198dbf89a
Ömer Faruk Aslan <86562106+omeraslandev@users.noreply.github.com>
2026-09-11 15:36:08 +0300

    deneme2

--- 2nd-year/csharp-programming-i/00/helloWorld.cs
+++ 2nd-year/csharp-programming-i/00/helloWorld.cs
@@ -8,8 +8,8 @@ namespace HelloWorld
 	public class Program
     	{
         		public static void Main(string[] args)
-                		{
-                        			Console.WriteLine("Hello, World!");
-                                    		}
-                                            	}
-                                                }
+                {
+                    Console.WriteLine("Hello, World!");
+                }
+        }
+}
